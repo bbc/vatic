470c0d60d64063c8626c804a928a66453375bc6a
Ben Clark <ben.clark02@bbc.co.uk>
2016-12-02 15:53:29 +0100

    Add static data for testing

--- public/bootstrap.js
+++ public/bootstrap.js
@@ -3,6 +3,24 @@ var development = false;
 var container;
 
 $(document).ready(function() {
+    data = {
+      "jobid":"100",
+      "slug":"slug",
+      "start":"0",
+      "stop":"100",
+      "width":"704",
+      "height":"576",
+      "skip":"25",
+      "perobject":"5", // TODO remove later
+      "completion":"100", // TODO remove later
+      "blowradius":"5", // TODO remove later?
+      "labels":"[\"cat\",\"dog\",\"fish\"]",
+      "attributes":"[\"happy\",\"sad\"]",
+      "training":"0", // TODO remove later
+      "verified":"1", // TODO remove later
+      "data":"[{\"label\",\"cat 1\",\"attributes\":[],\"boxes\":[]}]"
+    }
+
     console.log("Booting...");
 
     container = $("#container");
