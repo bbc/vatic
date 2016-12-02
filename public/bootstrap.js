d58147fed6f022807d6be1a38fe45d99b99dadf8
Ben Clark <ben.clark02@bbc.co.uk>
2016-12-02 16:03:12 +0100

    Simplify boot method

--- public/bootstrap.js
+++ public/bootstrap.js
@@ -25,51 +25,8 @@ $(document).ready(function() {
 
     container = $("#container");
 
-    if (isIE())
-    {
-        container.html("<p style='width:500px;'><strong>Sorry!</strong> This application does not currently support Internet Explorer. Please upgrade to a more modern browser to complete this HIT. We recommend <a href='http://www.google.com/chrome' target='_blank'>Google Chrome</a> or <a href='http://www.getfirefox.com' target='_blank'>Mozilla Firefox</a>.</p>");
-        return;
-    }
-
-    var parameters = mturk_parameters();
-    if (!parameters["id"])
-    {
-        brandingscreen();
-        return;
-    }
-
-    if (!mturk_isassigned())
-    {
-        mturk_acceptfirst();
-    }
-    else
-    {
-        mturk_showstatistics();
-    }
-
-    mturk_disabletimer();
-
-    function dispatch(training)
-    {
-        training = training ? 1 : 0;
-        server_request("getjob", [parameters["id"], training], function(data) {
-            loadingscreen(job_import(data));
-        });
-    }
-
-    worker_isverified(function() {
-        console.log("Worker is verified");
-        dispatch(false);
-    }, function() {
-        console.log("Worker is NOT verified");
-        dispatch(true);
-    });
+    loadingscreen(job_import(data));
 });
-
-function isIE () {
-  var myNav = navigator.userAgent.toLowerCase();
-  return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
-}
 
 function loadingscreen(job)
 {
@@ -132,18 +89,4 @@ function loadingscreen(job)
             }
         })
     );
-}
-
-function brandingscreen()
-{
-    console.log("Loading screen");
-    var d = $("<div style='margin:0 auto;width:400px;background-color:#fff;padding:20px;'></div>");
-    d.append("<h1>Welcome to vatic</h1>");
-    d.append("<p><strong>vatic</strong> is an online, interactive video annotation tool for computer vision research that crowdsources work to Amazon's Mechanical Turk. Our tool makes it easy to build massive, affordable video data sets and can be deployed on a cloud. Written in Python + C + Javascript, vatic is free and open-source software.</p>");
-    d.append("<p><a href='http://mit.edu/vondrick/vatic' target='_blank'>More information &raquo;</a></p>");
-    d.hide();
-    d.appendTo(container);
-    d.show("explode", 1000);
-
-    $("body").animate({"background-color": "#666"}, 1000);
 }
